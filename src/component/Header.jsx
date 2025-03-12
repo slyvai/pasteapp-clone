0cffaf2c60f829248671b012299323a83b268b73
slyvia <slyviafams@gmail.com>
2025-03-12 07:47:26 +0700

    second-commit

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -7,19 +7,31 @@ function Header () {
   const header2 = useRef(null);
   const image1 = useRef(null);
   const image2 = useRef(null);
+  const image3 = useRef(null)
 
 
   useEffect(() => {
-    const text = [header1.current, header2.current, image1.current, image2.current];
+    const text = [header1.current, header2.current, image1.current, image2.current, image3.current];
     const tl = gsap.timeline();
+
     tl.set(text, {y: 50, opacity: 0})
-    tl.to(header1.current, {y: -40, opacity: 1, duration: 0.7,  ease: "power1.in"}),
-    tl.to(header1.current, {y:  10, opacity: 0, duration: 0.7,  ease: "power1.out"})
-    tl.to(header2.current, {y: -40, opacity: 1, duration: 0.7, ease: "power1.in"})
-    tl.to(header2.current, {y:  10, opacity: 0, duration: 0.7,  ease: "power1.out"})
-    tl.to(image1.current, {y: 0, opacity: 1, scale: 0.8, duration: 0.7,  ease: "power1.out"})
-    tl.to(image1.current, {y: 0, opacity: 1, scale: 0.2 ,  ease: "power1.out"})
-    tl.to(image2.current, {y: 0, opacity: 1, scale: 1.5, zIndex: -1,})
+    tl.to(header1.current, {y: -40, opacity: 1, duration: 1,  ease: "back.inOut"})
+    tl.to(header1.current, {y:  -80, opacity: 0, duration: 0.8,  ease: "back.out"})
+    tl.to(header2.current, {y: -40, opacity: 1, duration: 1, ease: "back.inOut"})
+    tl.to(header2.current, {y:  -80, opacity: 0, duration: 0.8,  ease: "back.out"})
+
+    tl.set(image1.current, {y: 0, opacity: 1}) 
+    tl.to(image1.current, {scale: 1, ease: "power1"})
+    tl.to(image1.current, {scale: 0.2})
+    tl.to(image1.current, {scale: 0.3})
+    tl.to(image1.current, {opacity: 0, duration:0.4})
+
+
+    tl.set(image2.current, { scale: 5.5, y: 0, x: 0, opacity: 1, });
+    tl.to(image2.current, {scale: 0.4, y: 0, y: 0, opacity:1, duration: 2 })
+    tl.set(image3.current, {scale : 1.5, opacity:1})
+
+    
   }, [])
 
   return (
@@ -32,10 +44,14 @@ function Header () {
         <b>Once Again</b>
       </div>
       <div className="header-image-1" ref={image1} style={{position: "absolute"}}>
-      <img src="https://framerusercontent.com/modules/c8mJrTPIqkx5l0MUgmyY/ezXUcutlEwTxfTpyBQxd/assets/9aV5yKF0n3MKIlDKf0iVHhuaHc.png" alt="" />
+      <img src="https://framerusercontent.com/modules/c8mJrTPIqkx5l0MUgmyY/ezXUcutlEwTxfTpyBQxd/assets/JGv9jLVFG8n8Q8LmB1PFIpzbZo.png" alt="" />
       </div>
 
       <div className="header-image-2" ref={image2} style={{position: "absolute"}}>
+        <img src="https://framerusercontent.com/modules/c8mJrTPIqkx5l0MUgmyY/ezXUcutlEwTxfTpyBQxd/assets/512/9aV5yKF0n3MKIlDKf0iVHhuaHc.png" alt="" />
+      </div>
+
+      <div className="header-image-3" ref={image3}>
         <img src="https://framerusercontent.com/images/DhDsfyks1iwAWu29uci5zpO8.jpg" alt="" />
       </div>
     </div>
--- src/component/HoverComponent.jsx
+++ src/component/HoverComponent.jsx
@@ -1,66 +1,133 @@
-import { useEffect } from "react";
-import gsap from "gsap";
-import "../style/HoverComponent.css"
+  import { useEffect } from "react";
+  import gsap from "gsap";
+  import { ScrollTrigger } from "gsap/all";
+  import "../style/HoverComponent.css"
 
-function HoverComponent() {
+  gsap.registerPlugin(ScrollTrigger);
 
-  const images = [
-    "https://framerusercontent.com/images/j2KSfMROOnRMxthTZTxFhWU2ZEk.png?scale-down-to=512",
-    "https://framerusercontent.com/images/LRW8TpgnamqDbqIeLH4YFIizo.png?scale-down-to=512",
-    "https://framerusercontent.com/images/MHdRNTvXt7C56gNdk7vxdhhVUMQ.png?scale-down-to=512",
-    "https://framerusercontent.com/images/tjFslIrii2zAbgwpgv88acKX94.png?scale-down-to=512",
-    "https://framerusercontent.com/images/mOjeWW0R85ST6vSbp4NsqVp21k.png?scale-down-to=512",
-    "https://framerusercontent.com/images/QHkLOR1e2hPGvIhTMAOeLf0iiI.png?scale-down-to=512",
-    "https://framerusercontent.com/images/BaoJlOV474hfTXenp7mqpGnN7A.png?scale-down-to=512",
-    "https://framerusercontent.com/images/BuegvK1yhbKVL0nuVa01UneIrU.png?scale-down-to=512",
-    "https://framerusercontent.com/images/Oc81fd2Au71f2UmpMtKlhZcQcj0.png?scale-down-to=512",
-    "https://framerusercontent.com/images/EWguGk5v4KfiMZIncNMoVZyUfF4.png?scale-down-to=512",
-    "https://framerusercontent.com/images/zVEmxPBCyTF0qlp2x4NnROYZvM.png?scale-down-to=512",
-    "https://framerusercontent.com/images/eHdMlQZUjVqhhiT0dZE8onugIc0.png?scale-down-to=512",
-    "https://framerusercontent.com/images/oIWj5rD83R2smndIfKHeVdv5A.png?scale-down-to=512",
-  ];
+  function HoverComponent() {
 
-  useEffect(() => {
-    const images = document.querySelectorAll(".hover-image");
+    const images = [
+      "https://framerusercontent.com/images/j2KSfMROOnRMxthTZTxFhWU2ZEk.png?scale-down-to=512",
+      "https://framerusercontent.com/images/LRW8TpgnamqDbqIeLH4YFIizo.png?scale-down-to=512",
+      "https://framerusercontent.com/images/MHdRNTvXt7C56gNdk7vxdhhVUMQ.png?scale-down-to=512",
+      "https://framerusercontent.com/images/tjFslIrii2zAbgwpgv88acKX94.png?scale-down-to=512",
+      "https://framerusercontent.com/images/mOjeWW0R85ST6vSbp4NsqVp21k.png?scale-down-to=512",
+      "https://framerusercontent.com/images/QHkLOR1e2hPGvIhTMAOeLf0iiI.png?scale-down-to=512",
+      "https://framerusercontent.com/images/BaoJlOV474hfTXenp7mqpGnN7A.png?scale-down-to=512",
+      "https://framerusercontent.com/images/BuegvK1yhbKVL0nuVa01UneIrU.png?scale-down-to=512",
+      "https://framerusercontent.com/images/Oc81fd2Au71f2UmpMtKlhZcQcj0.png?scale-down-to=512",
+      "https://framerusercontent.com/images/EWguGk5v4KfiMZIncNMoVZyUfF4.png?scale-down-to=512",
+      "https://framerusercontent.com/images/zVEmxPBCyTF0qlp2x4NnROYZvM.png?scale-down-to=512",
+      "https://framerusercontent.com/images/eHdMlQZUjVqhhiT0dZE8onugIc0.png?scale-down-to=512",
+      "https://framerusercontent.com/images/oIWj5rD83R2smndIfKHeVdv5A.png?scale-down-to=512",
+    ];
 
-    images.forEach((image) => {
-      const handleEnter = () => {
-        gsap.to(image, { y: -100});
-      };
+    useEffect(() => {
+      const container = document.querySelector(".hover-parent-content")
 
-      const handleLeave = () => {
-        gsap.to(image, { y: 0 });
-      };
+      gsap.fromTo(container, {y:0}, 
+        {y: 100, duration: 1, ease: "bounce.out"}, {
+          scrollTrigger: {
+            trigger: container,
+            start: "top top",
+            end: "bottom top",
+            scrub: true,
+          }
+        }
+      )
+    }, [])
 
-      image.addEventListener("mouseenter", handleEnter);
-      image.addEventListener("mouseleave", handleLeave);
+    useEffect(() => {
+      const images = document.querySelectorAll(".hover-image");
 
-  
-      return () => {
-        image.removeEventListener("mouseenter", handleEnter);
-        image.removeEventListener("mouseleave", handleLeave);
-      };
-    });
-  }, []);
+      images.forEach((image) => {
+        const handleEnter = () => {
+          gsap.to(image, { y: -100});
+        };
 
- return (
-   <>
-   <div className="hover-title">
-      <h1>Fresh New Look <span className="hover-title-span">Elegance Infused With Efficiency</span></h1>
-      <p>Every pixel has been refined and polished with care, resulting  <br />in a harmonious balance between aesthetics and usability.</p>
+        const handleLeave = () => {
+          gsap.to(image, { y: 0 });
+        };
+
+        image.addEventListener("mouseenter", handleEnter);
+        image.addEventListener("mouseleave", handleLeave);
+
+    
+        return () => {
+          image.removeEventListener("mouseenter", handleEnter);
+          image.removeEventListener("mouseleave", handleLeave);
+        };
+      });
+    }, []);
+
+  return (
+    <>
+    <div className="hover-title">
+        <h1>Fresh New Look <span className="hover-title-span">Elegance Infused With Efficiency</span></h1>
+        <p>Every pixel has been refined and polished with care, resulting  <br />in a harmonious balance between aesthetics and usability.</p>
+      </div>
+    <div className="hover-container">
+      {images.map((image, index) => (
+        <img
+          key={index}
+          src={image}
+          alt={`Image ${index}`}
+          className="hover-image"
+        />
+      ))}
     </div>
-   <div className="hover-container">
-    {images.map((image, index) => (
-      <img
-        key={index}
-        src={image}
-        alt={`Image ${index}`}
-        className="hover-image"
-      />
-    ))}
-   </div>
-   </>
- )
-}
 
-export default HoverComponent;
+    <div className="content-container">
+    <div className="hover-content" style={{ position: "relative" }}>
+      <img
+        src="https://framerusercontent.com/images/2KUy0UQ0uRhfYn5bVZ19ei1HS68.jpg?scale-down-to=512"
+        style={{ width: "150%", height: "auto" }}
+        alt=""
+      />
+      <div className="hover-overlay" style={{ position: "absolute", top: 0, left: 0 }}>
+        <h1>Effiency <br /> at the Core</h1>
+        <p>
+          We've prioritized clarity, revamping <br />
+          all UI elements for seamless and intuitive <br />
+          interactions.
+        </p>
+        <img
+          src="https://framerusercontent.com/images/Ibk1ExBnJO3opjVjfXYS9KPQo.png"
+          alt=""
+          style={{ width: "400px", height: "400px" }}
+        />
+      </div>
+    </div>
+
+  <div className="hover-parent-content">
+    <div className="hover-content1">
+      <img
+        src="https://framerusercontent.com/images/ftNO3NM11s4GTVGuWkhs3DMspw.png?scale-down-to=512"
+        alt=""
+      />
+    </div>
+
+    <div className="hover-content1-child">
+      <p>The new color palette is optimized for both light and dark themes.</p> <br />
+      <img
+        src="https://framerusercontent.com/images/4wt7dX5Q9uGUe8m2ucCIZLIfg9w.jpg"
+        alt=""
+      />
+    </div>
+    </div>
+  </div>
+  <div className="content-end">
+    <div className="content-end-child">
+    <div className="content-end-title">
+    <h1>Accuracy <br /> and Precision</h1>
+    <p>No unnecessary indents to create more space for your content.</p>
+    </div>
+    <img src="https://framerusercontent.com/images/xg6GGbjhCfddoWBwDBAiJ42n1U.jpg?scale-down-to=512" alt="" />
+    </div>
+  </div>
+    </>
+  )
+  }
+
+  export default HoverComponent;
